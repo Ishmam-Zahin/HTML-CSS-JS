d9ccc54550ef8a741040c8a1cc48e0b2c28016be
zahin <mdishmamzahin@gmail.com>
2024-01-03 16:28:22 +0600

    initial

---
+++ home.js
@@ -0,0 +1,5 @@
+function clearField(value, element){
+    if(element.value == value) element.value = "";
+    else element.value = element.value;
+    //console.log(element.value);
+}
---
+++ java.js
@@ -0,0 +1,11 @@
+function myfunc(){
+    console.log("what!");
+    document.getElementById("x").innerHTML = "my name";
+}
+function myfunc2(){
+    document.getElementById("x").innerHTML = "xxx"
+}
+
+function clearrr(){
+    document.getElementById("xx").value = "";
+}
